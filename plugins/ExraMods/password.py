63d58050aa55c8962c0359a3a0751fa9b330c53a
trapperboy <92577580+trapperboy@users.noreply.github.com>
2023-01-13 13:10:21 +0530

    Update password.py

--- plugins/ExraMods/password.py
+++ plugins/ExraMods/password.py
@@ -12,5 +12,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Movies, url='https://t.me/moviesbot_1')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Mmovies bot', url='https://t.me/MsBotTg1')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
